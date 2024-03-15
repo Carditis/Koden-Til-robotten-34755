289e2213bf56953036573b2b016ab48a2d16b34a
VictorTramDTU <62642279+VictorTramDTU@users.noreply.github.com>
2024-03-15 14:57:52 +0100

    Update bplan301.h

--- local/svn/robobot/raubase/src/bplan301.h
+++ local/svn/robobot/raubase/src/bplan301.h
@@ -1,24 +1,3 @@
-/*  
- * 
- * Copyright © 2023 DTU, Christian Andersen jcan@dtu.dk
- * 
- * The MIT License (MIT)  https://mit-license.org/
- * 
- * Permission is hereby granted, free of charge, to any person obtaining a copy of this software
- * and associated documentation files (the “Software”), to deal in the Software without restriction, 
- * including without limitation the rights to use, copy, modify, merge, publish, distribute, 
- * sublicense, and/or sell copies of the Software, and to permit persons to whom the Software 
- * is furnished to do so, subject to the following conditions:
- * 
- * The above copyright notice and this permission notice shall be included in all copies 
- * or substantial portions of the Software.
- * 
- * THE SOFTWARE IS PROVIDED “AS IS”, WITHOUT WARRANTY OF ANY KIND, EXPRESS OR IMPLIED, 
- * INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY, FITNESS FOR A PARTICULAR 
- * PURPOSE AND NONINFRINGEMENT. IN NO EVENT SHALL THE AUTHORS OR COPYRIGHT HOLDERS BE LIABLE 
- * FOR ANY CLAIM, DAMAGES OR OTHER LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, 
- * ARISING FROM, OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN 
- * THE SOFTWARE. */
 
 // aaa
 #pragma once
@@ -30,12 +9,12 @@ using namespace std;
  * Class intended to accomplish a short mission,
  * e.g. one challenge or part of a challenge
  * */
-class BPlan100
+class BPlan301
 {
 public:
   /**
    * destructor */
-    ~BPlan100();
+    ~BPlan301();
   /** setup and request data */
   void setup();
   /**
@@ -61,4 +40,4 @@ private:
 
 /**
  * Make this visible to the rest of the software */
-extern BPlan100 plan100;
+extern BPlan301 plan301;
